ed3de165176c60aea4415fc2455840fd81d9d40b
PauloScapol <paulo.scapol2@hotmail.com>
2023-02-06 13:11:22 -0300

    Esqueci de dar (git add .) para adicionar o componente AlbumDisplay.js

---
+++ src/components/AlbumDisplay.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class AlbumDisplay extends React.Component {
+  render() {
+    const { albumImg, artistName, artistSearch, collectionId } = this.props;
+
+    return (
+      <div>
+        <img src={ albumImg } alt={ artistSearch } />
+        <h3>{ artistSearch }</h3>
+        <h4>{ artistName }</h4>
+
+        <Link
+          data-testid={ `link-to-album-${collectionId}` }
+          to={ `/album/${collectionId}` }
+        >
+          + Info
+        </Link>
+      </div>
+    );
+  }
+}
+
+AlbumDisplay.propTypes = {
+  albumImg: PropTypes.string,
+  artistName: PropTypes.string,
+  artistSearch: PropTypes.string,
+  collectionId: PropTypes.string,
+}.isRequired;
+
+export default AlbumDisplay;
